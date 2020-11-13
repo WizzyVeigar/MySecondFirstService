7c38794f45bf8be5e6f9c9e32cb75e78de3314a3
Kenneth Folke Larsen <5504355@RTS-365.dk>
2020-11-13 11:54:43 +0100

    Add project files.

---
+++ MyFirstTestWindowsService/Library.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstTestWindowsService
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception e)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + e.Source.ToString().Trim() + "; " + e.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static void WriteErrorLog(string message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
